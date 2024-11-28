6c93febb4c584638e7553fdad9f216892a493b02
stevenhr21 <stevenhr21@gmail.com>
2024-11-28 20:26:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Initialize Leaflet map
 const map = L.map('map').setView([51.5074, -0.1278], 10); // Centered on London
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors',
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
