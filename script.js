7b70f64c18480747d913048ed1db57410503bc2c
stevenhr21 <stevenhr21@gmail.com>
2024-11-28 20:23:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,4 @@
 const map = L.map('map').setView([51.5074, -0.1278], 10); // Centered on London
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
-    maxZoom: 19,
 }).addTo(map);
